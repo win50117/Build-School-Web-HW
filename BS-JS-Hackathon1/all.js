291f1c805dd39258fa1a0f9c50362fb12895ba91
Cheng Ching Jen <win50117@gmail.com>
2020-07-30 20:49:16 +0800

    修改

--- BS-JS-Hackathon1/all.js
+++ BS-JS-Hackathon1/all.js
@@ -74,34 +74,46 @@ function initMap() {
     });
 }
 
-let xhr = new XMLHttpRequest();
+// let xhr = new XMLHttpRequest();
 const url =
     "https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json?fbclid=IwAR03QVRRMDHgQC_XBIR62wBKePkGVs5kRyTMdaCpP032CjtFdu6uiA3m-Gc";
 
-xhr.onload = function () {
-    maskdata = JSON.parse(this.responseText);
-    initMap();
-    addCountyList();
-};
-xhr.open("GET", url);
-xhr.send();
+// xhr.onload = function () {
+//     maskdata = JSON.parse(this.responseText);
+//     initMap();
+//     addCountyList();
+// };
+// xhr.open("GET", url);
+// xhr.send();
 
-function setPoint() {
-    maskdata.features.forEach(function (item) {
-        let latP = Number(item.geometry.coordinates[1]);
-        let lngP = Number(item.geometry.coordinates[0]);
-        let titleP = item.properties.name;
-        let maskLeftP = item.properties.masksLeft;
-        let childMaskLeftP = item.properties.childMasksLeft;
-        points.push({
-            lat: latP,
-            lng: lngP,
-            title: titleP,
-            maskLeft: maskLeftP,
-            childMaskLeft: childMaskLeftP,
-        });
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        maskdata = data;
+        initMap();
+        addCountyList();
+        initAutocomplete();
+    })
+    .catch((error) => {
+        alert(error);
     });
-}
+
+// function setPoint() {
+//     maskdata.features.forEach(function (item) {
+//         let latP = Number(item.geometry.coordinates[1]);
+//         let lngP = Number(item.geometry.coordinates[0]);
+//         let titleP = item.properties.name;
+//         let maskLeftP = item.properties.masksLeft;
+//         let childMaskLeftP = item.properties.childMasksLeft;
+//         points.push({
+//             lat: latP,
+//             lng: lngP,
+//             title: titleP,
+//             maskLeft: maskLeftP,
+//             childMaskLeft: childMaskLeftP,
+//         });
+//     });
+// }
 
 //縣市選單
 let countySelector = document.querySelector(".selectCounty");
@@ -192,4 +204,67 @@ function renderList(city, town) {
             list.append(cloneContent);
         }
     }
+}
+
+function initAutocomplete() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -33.8688, lng: 151.2195 },
+        zoom: 13,
+        mapTypeId: "roadmap",
+    });
+    // Create the search box and link it to the UI element.
+    const input = document.getElementById("pac-input");
+    const searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener("bounds_changed", () => {
+        searchBox.setBounds(map.getBounds());
+    });
+    let markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener("places_changed", () => {
+        const places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+        // Clear out the old markers.
+        markers.forEach((marker) => {
+            marker.setMap(null);
+        });
+        markers = [];
+        // For each place, get the icon, name and location.
+        const bounds = new google.maps.LatLngBounds();
+        places.forEach((place) => {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            const icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25),
+            };
+            // Create a marker for each place.
+            markers.push(
+                new google.maps.Marker({
+                    map,
+                    icon,
+                    title: place.name,
+                    position: place.geometry.location,
+                })
+            );
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
 }
