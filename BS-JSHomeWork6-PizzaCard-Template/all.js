c55565c8267b1f0ab4af2ddd84fdb8b98438f463
win50117 <win50117@gmail.com>
2020-08-02 04:10:04 +0800

    更新內容、排版

--- BS-JSHomeWork6-PizzaCard-Template/all.js
+++ BS-JSHomeWork6-PizzaCard-Template/all.js
@@ -9,11 +9,39 @@ let pizzaObject;
 function setCard() {
     let card = $g("#cardPizza");
     for (let i = 0; i < pizzaObject.products.length; i++) {
+        let foodary = pizzaObject.products[i].food;
+
         let cloneContent = card.content.cloneNode(true);
         cloneContent.querySelector("h5.card-title").innerText =
             pizzaObject.products[i].name;
         cloneContent.querySelector("h5.card-eng-title").innerText =
             pizzaObject.products[i].engname;
+
+        for (let item of foodary) {
+            cloneContent.querySelector(".food").innerHTML +=
+                item == "pig"
+                    ? '<i class="fas fa-piggy-bank"></i>'
+                    : item == "chicken"
+                    ? '<i class="fas fa-drumstick-bite"></i>'
+                    : item == "fish"
+                    ? '<i class="fas fa-fish"></i>'
+                    : item == "vegetables"
+                    ? '<i class="fas fa-seedling"></i>'
+                    : item == "smhot"
+                    ? '<i class="fas fa-pepper-hot"></i>'
+                    : item == "mdhot"
+                    ? '<i class="fas fa-pepper-hot"></i><i class="fas fa-pepper-hot"></i>'
+                    : item;
+        }
+
+        cloneContent.querySelector(".price").innerHTML +=
+            `<span class="word">L</span><span class="pizzaprice">$${pizzaObject.products[i].lprice}</span>` +
+            `${
+                pizzaObject.products[i].sprice != 0
+                    ? `<span class="word">S</span><span class="pizzaprice">$${pizzaObject.products[i].sprice}</span>`
+                    : ""
+            }`;
+
         cloneContent.querySelector("img").src = pizzaObject.products[i].picture;
         cloneContent
             .querySelector(".btn")
