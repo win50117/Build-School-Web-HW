b3827bf73779fb42a61f28827c692d7fc3128c58
win50117 <win50117@gmail.com>
2020-08-07 22:12:52 +0800

    行事曆初版完成

--- BS-JS-Hackathon2-Calendar/all.js
+++ BS-JS-Hackathon2-Calendar/all.js
@@ -2,6 +2,8 @@ let localdate = new Date();
 let nowYear = localdate.getFullYear();
 let nowMonth = localdate.getMonth();
 let nowDay = localdate.getDate();
+let currentIndex;
+let currentDate;
 
 let calendarYear = document.querySelector(".calendar-year");
 calendarYear.innerText = nowYear;
@@ -33,9 +35,6 @@ document.querySelector(".next").addEventListener("click", function () {
     setTbodyDay();
 });
 
-//存放localStorage資料
-let todoItem = [];
-
 function init() {
     setTbodyDay();
 }
@@ -64,62 +63,34 @@ function setTbodyDay() {
     let i = 0;
     while (i < allDays) {
         let tr = document.createElement("tr");
+        let nextDate = 1;
         for (let row = 0; row < 7; row++) {
             //填補前面的空格
             if (firstrow == false) {
+                let predate = new Date(nowYear, nowMonth, 0);
+                //取得上個月一共有幾天 減去本月第一天星期-1 就是顯示上月的起始日期
+                let allPreDate = predate.getDate();
+                allPreDate -= firstDate - 1;
                 for (let j = 0; j < firstDate; j++) {
                     let td = document.createElement("td");
+                    td.innerText = allPreDate;
+                    allPreDate++;
+                    td.style.backgroundColor = "#eee";
                     tr.appendChild(td);
                     row++;
                 }
                 firstrow = true;
             }
             let td = document.createElement("td");
-            /////////////////////////助教code 參考/////////////////////////////
-            // if (localStorage.getItem(`${year}-${month + 1}-${day}`) != null) {
-            //     let todoList = JSON.parse(
-            //         localStorage.getItem(`${year}-${month + 1}-${day}`)
-            //     );
-
-            //     let ul = document.createElement("ul");
-
-            //     todoList.forEach((element, index) => {
-            //         let li = document.createElement("li");
-            //         li.innerText = element.title;
-            //         li.setAttribute("index", index);
-
-            //         li.addEventListener("click", function (e) {
-            //             e.stopPropagation();
-
-            //             let index = e.target.getAttributeNode("index").value;
-            //             console.log(index);
-            //             currentIndex = index;
-            //             let date = `${year}-${month + 1}-${
-            //                 e.target.offsetParent.childNodes[0].data
-            //             }`;
-            //             console.log(date);
-
-            //             let todoItem = JSON.parse(localStorage.getItem(date))[
-            //                 index
-            //             ];
-            //             console.log(todoItem);
-
-            //             document.querySelector(
-            //                 "#info-area #info-date"
-            //             ).value = `${date}`;
-            //             document.querySelector(
-            //                 "#info-area #info-todo-item"
-            //             ).value = `${todoItem.title}`;
-
-            //             $("#infoModal").modal("show");
-            //         });
-            //         ul.appendChild(li);
-            //     });
-
-            //     td.appendChild(ul);
-            // }
-            //////////////////////////////////////////////////////
-
+            //填補後面的空格
+            if (i >= allDays) {
+                td.innerText = nextDate;
+                nextDate++;
+                td.style.backgroundColor = "#eee";
+                tr.appendChild(td);
+                i++;
+                continue;
+            }
             td.innerText = i + 1;
 
             td.setAttribute("data-date", `${nowYear}-${nowMonth}-${i + 1}`);
@@ -129,19 +100,55 @@ function setTbodyDay() {
             td.addEventListener("click", function () {
                 setTodo(this);
             });
+            //////////////////////////////////////////////////////
+            if (
+                localStorage.getItem(`${nowYear}-${nowMonth}-${i + 1}`) != null
+            ) {
+                let todoList = JSON.parse(
+                    localStorage.getItem(`${nowYear}-${nowMonth}-${i + 1}`)
+                );
+                let ul = document.createElement("ul");
+
+                todoList.forEach((element, index) => {
+                    // console.log(element);
+                    let li = document.createElement("li");
+                    li.innerText = element.title;
+                    li.style.backgroundColor = element.color;
+                    li.setAttribute("index", index);
+
+                    //代辦事項li標題點擊事件
+                    li.addEventListener("click", function (e) {
+                        e.stopPropagation();
+
+                        currentIndex = index;
+                        currentDate = td.dataset.date;
+
+                        document.querySelector(
+                            "#todoModal .todo-title"
+                        ).value = `${element.title}`;
+                        document.querySelector(
+                            "#info-area .todo-things"
+                        ).value = `${element.things}`;
+                        document.querySelector(
+                            "#info-area .todo-color"
+                        ).value = `${element.color}`;
+                        $("#todoModal").modal("show");
+                    });
+                    //如果index大於3 建立總清單
+                    ul.appendChild(li);
+                });
+
+                td.appendChild(ul);
+            }
+
+            //////////////////////////////////////////////////////
             tr.appendChild(td);
             i++;
-            if (i == allDays) {
-                break;
-            }
         }
         tbody.appendChild(tr);
     }
 }
-// year: [],
-// month: [],
-// date: [],
-// things: [],
+
 let thisTdEl;
 function setTodo(thisTd) {
     thisTd.setAttribute("data-toggle", "modal");
@@ -151,7 +158,7 @@ function setTodo(thisTd) {
     nowYear = localdate.getFullYear();
     nowMonth = localdate.getMonth();
     nowDay = thisTd.dataset.day;
-    console.log(thisTd);
+    // console.log(thisTd);
 }
 
 let btnsaveTodo = document.querySelector(".btn-todo-save");
@@ -166,54 +173,60 @@ function btnSaveTodof() {
     let yearMonthDay = `${nowYear}-${nowMonth}-${nowDay}`;
 
     saveToLocalStorage(yearMonthDay, inputTitle, inputThings, inputColor);
-    showTodoList(yearMonthDay, inputTitle, inputThings, inputColor);
 }
 
 function saveToLocalStorage(yearMonthDay, inputTitle, inputThings, inputColor) {
-    todoItem.push({
+    let todoObj = {
         title: inputTitle,
         things: inputThings,
         color: inputColor,
-    });
-    localStorage.setItem(yearMonthDay, JSON.stringify(todoItem));
+    };
+    //存放localStorage資料
+    let todoList = [];
+    if (localStorage.getItem(yearMonthDay) == null) {
+        todoList.push(todoObj);
+    } else {
+        let alreadyTodoList = JSON.parse(localStorage.getItem(yearMonthDay));
+        alreadyTodoList.push(todoObj);
+        todoList = alreadyTodoList;
+    }
+    localStorage.setItem(yearMonthDay, JSON.stringify(todoList));
+    //重新渲染表格
+    setTbodyDay();
 }
 
-function showTodoList(yearMonthDay, inputTitle, inputThings, inputColor) {
-    let todoObject1 = JSON.parse(localStorage.getItem(yearMonthDay));
-
-    console.log(todoObject1);
-
-    let p = document.createElement("p");
-
-    p.setAttribute("class", "todo-item");
-    p.innerText = inputTitle;
-    p.style.color = inputColor;
-    p.addEventListener("click", function (e) {
-        console.log(e);
-        console.log(
-            nowYear,
-            nowMonth,
-            e.target.offsetParent.childNodes[0].textContent
-        );
-        //彈出modal視窗
-        e.stopPropagation();
-        $("#todoModal").modal("show");
-        //取得localStorage資料
-        let todoObject = JSON.parse(
-            localStorage.getItem(
-                `${nowYear}-${nowMonth}-${e.target.offsetParent.childNodes[0].textContent}`
-            )
-        );
-        console.log(todoObject);
-        // document.querySelector();
-        // p.style.color = todoObject[0].color;
-
-        document.querySelector("#todoModal .todo-title").innerText =
-            todoObject[0].title;
-        document.querySelector("#todoModal .todo-things").innerText =
-            todoObject[0].things;
-        document.querySelector("#todoModal .todo-color").innerText =
-            todoObject[0].color;
+document
+    .querySelector("#todoModal .btn-todo-edit")
+    .addEventListener("click", function () {
+        // console.log(currentIndex);
+        // console.log(currentDate);
+        let inputTitle = document.querySelector("#todoModal .todo-title").value;
+        let inputThings = document.querySelector("#todoModal .todo-things")
+            .value;
+        let inputColor = document.querySelector("#todoModal .todo-color").value;
+        let todoList = JSON.parse(localStorage.getItem(currentDate));
+        let todoObj = {
+            title: inputTitle,
+            things: inputThings,
+            color: inputColor,
+        };
+        //把原本位置的todoObj刪除，插入修改後的todoObj
+        todoList.splice(currentIndex, 1, todoObj);
+        //重新把資料取代回localStorage
+        localStorage.setItem(currentDate, JSON.stringify(todoList));
+        //重新渲染表格
+        setTbodyDay();
     });
-    thisTdEl.appendChild(p);
-}
+
+document
+    .querySelector("#todoModal .btn-todo-delete")
+    .addEventListener("click", function () {
+        let todoList = JSON.parse(localStorage.getItem(currentDate));
+        todoList.splice(currentIndex, 1);
+        if (todoList.length === 0) {
+            localStorage.removeItem(currentDate);
+        } else {
+            localStorage.setItem(currentDate, JSON.stringify(todoList));
+        }
+        setTbodyDay();
+    });
