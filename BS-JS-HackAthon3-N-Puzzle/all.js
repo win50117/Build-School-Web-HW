41f5be172ea46d8e4fe627acfad3b03d9e00ddf4
win50117 <win50117@gmail.com>
2020-08-11 02:46:57 +0800

    變數宣告完成

--- BS-JS-HackAthon3-N-Puzzle/all.js
+++ BS-JS-HackAthon3-N-Puzzle/all.js
@@ -28,8 +28,47 @@ whiteImg.src = "img/white.png";
 //---取得父元素
 var puzzleArea = document.getElementById("puzzle-area");
 
+let baseBlock = [0, 1, 2, 3, 4, 5, 6, 7, 8, 0]; //大區塊的編號，index0不使用第9為空白格 3*3
+let baseBlock22 = [0, 1, 2, 3, 0];
+//每個區塊可移動的位置
+let blockCanMove = new Array(
+    [0], //不使用0的陣列位置，比較好判斷
+    [2, 4], //區塊1可以移動到區塊2、4的位置
+    [1, 3, 5], //區塊2可以移動到區塊1、3、5的位置
+    [2, 6],
+    [1, 5, 7],
+    [2, 4, 6, 8],
+    [3, 5, 9],
+    [4, 8],
+    [5, 7, 9],
+    [6, 8]
+);
+
+let blockCanMove22 = new Array(
+    [0], //不使用0的陣列位置，比較好判斷
+    [2, 3], //區塊1可以移動到區塊2、3的位置
+    [1, 4], //區塊2可以移動到區塊1、4的位置
+    [1, 4],
+    [2, 3]
+);
+let posX; //座標寬
+let posY; //座標高
+
+let blockPosXY = new Array(
+    [0], //不使用index 0
+    [0, 0][(posX, 0)], //第一個表示left,第二個表示top，比如第一塊位置為left:0px ,top:0px
+    [posX * 2, 0],
+    [0, posY],
+    [posX, posY],
+    [posX * 2, posY],
+    [0, posY * 2],
+    [posX, posY * 2],
+    [posX * 2, posY * 2]
+);
+let b = [];
+
 //---設定幾乘幾
-let imgBase = 3;
+let imgBase = 2;
 
 puzzleImg.onload = function (e) {
     let maxWitdh = 540;
@@ -52,13 +91,17 @@ puzzleImg.onload = function (e) {
         }
     }
     ///////////////
+    let d = 1;
     for (let i = 0; i < imgBase; i++) {
         for (let j = 0; j < imgBase; j++) {
             let canvas = document.createElement("canvas");
             canvas.width = puzzleImg.width / imgBase;
             canvas.height = puzzleImg.height / imgBase;
+            posX = canvas.width; //之後的座標寬
+            posY = canvas.height; //之後的座標高
             canvasContent = canvas.getContext("2d");
-            if (i == 2 && j == 2) {
+            //白色區塊的位置
+            if (i == imgBase - 1 && j == imgBase - 1) {
                 //我先寫死
                 canvasContent.drawImage(
                     whiteImg,
@@ -72,6 +115,8 @@ puzzleImg.onload = function (e) {
                     pHeight
                 );
                 canvas.setAttribute("class", "white");
+                canvas.setAttribute("id", `b${imgBase * 2}`);
+                canvas.setAttribute("data-num", imgBase * 2);
             } else {
                 canvasContent.drawImage(
                     puzzleImg,
@@ -85,6 +130,9 @@ puzzleImg.onload = function (e) {
                     canvas.height
                 );
                 canvas.setAttribute("class", "moveable");
+                canvas.setAttribute("id", `b${d}`);
+                canvas.setAttribute("data-num", d);
+                d++;
             }
             puzzleArea.appendChild(canvas);
         }
