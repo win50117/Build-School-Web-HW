819a4a12287f9cad37f112bc0ef403b305008bde
win50117 <win50117@gmail.com>
2020-08-16 01:30:23 +0800

    洗亂拼圖完成

--- BS-JS-HackAthon3-N-Puzzle/all.js
+++ BS-JS-HackAthon3-N-Puzzle/all.js
@@ -1,5 +1,3 @@
-// var myCanvas = document.getElementById("myCanvas");
-
 // 上傳圖片
 document.querySelector(".file-input").addEventListener("change", readFile);
 
@@ -22,50 +20,11 @@ var puzzleImg = new Image();
 var whiteImg = new Image();
 whiteImg.src = "img/white.png";
 
-//---2. 生成切片
-//var puzzleItems = document.querySelectorAll('#puzzle-area canvas');
-//console.log(puzzleItems);
-
 //---取得父元素
 var puzzleArea = document.getElementById("puzzle-area");
 
-// let baseBlock22 = [0, 1, 2, 3, 0];
-// //每個區塊可移動的位置
-// let blockCanMove = new Array(
-//     [0], //不使用0的陣列位置，比較好判斷
-//     [2, 4], //區塊1可以移動到區塊2、4的位置
-//     [1, 3, 5], //區塊2可以移動到區塊1、3、5的位置
-//     [2, 6],
-//     [1, 5, 7],
-//     [2, 4, 6, 8],
-//     [3, 5, 9],
-//     [4, 8],
-//     [5, 7, 9],
-//     [6, 8]
-// );
-
-// let blockCanMove22 = new Array(
-//     [0], //不使用0的陣列位置，比較好判斷
-//     [2, 3], //區塊1可以移動到區塊2、3的位置
-//     [1, 4], //區塊2可以移動到區塊1、4的位置
-//     [1, 4],
-//     [2, 3]
-// );
 let AreaHeight; //座標寬
 let AreaWidth; //座標高
-
-// let blockPosXY = new Array(
-//     [0], //不使用index 0
-//     [0, 0][(posX, 0)], //第一個表示left,第二個表示top，比如第一塊位置為left:0px ,top:0px
-//     [posX * 2, 0],
-//     [0, posY],
-//     [posX, posY],
-//     [posX * 2, posY],
-//     [0, posY * 2],
-//     [posX, posY * 2],
-//     [posX * 2, posY * 2]
-// );
-// let b = [];
 
 //---設定幾乘幾
 let imgBase = 2;
@@ -169,26 +128,9 @@ puzzleImg.onload = function (e) {
     puzzleArea.style.width = `${AreaWidth * imgBase}px`;
     puzzleArea.style.height = `${AreaHeight * imgBase}px`;
     document.querySelector(".custom-file-input").disabled = true;
-    // addBlockEvent();
+    document.querySelector("#shuffleBtn").disabled = false;
+    document.querySelector("#startBtn").disabled = false;
 };
-
-//window onload ?? (當圖都長完才抓得到canvas)
-// function addBlockEvent() {
-//     var canvasBlocks = document.querySelectorAll(".moveable");
-//     console.log(canvasBlocks);
-
-//     for (let block of canvasBlocks) {
-//         block.addEventListener("click", function () {
-//             console.log(this.dataset.num);
-//             move(this.dataset.num);
-//             //判斷是否可以被移動
-//             //--1.判斷自己是否在白色塊上下左右???
-//             //移動方法
-//             //--1.白色塊與自己交換'位置'
-//             //判斷輸贏
-//         });
-//     }
-// }
 
 function move(clickId) {
     let row = imgBase;
@@ -196,12 +138,6 @@ function move(clickId) {
     let whiteBlock = document.querySelector(".white");
     let clickBLockIndex = localBlock.indexOf(Number(clickId));
     let whiteBlockIndex = localBlock.indexOf(Number(imgBase * imgBase - 1));
-    console.log(row);
-    console.log(clickBLockIndex);
-    console.log(clickBLockIndex / row);
-    console.log(clickBLockIndex + row);
-
-    console.log(whiteBlockIndex);
 
     if (
         //點擊方塊不在最上排，且上面是空白方塊，可向上移動
@@ -265,18 +201,62 @@ function checkWinGame() {
     }
 }
 
-// 兩個陣列一個保存答案的固定位置
-// 另一個保存要移動的格子位置
+//洗亂按鈕事件
+document.querySelector("#shuffleBtn").addEventListener("click", function () {
+    let row = imgBase;
+    let whiteBlock = document.querySelector(".white");
 
-// 移動判斷陣列0位置
-// row等於總數/幾成幾數字
+    let canvas = document.querySelectorAll("canvas");
 
-// 可移動的空白格子上方就是 空白格字的index-row index從0開始
-// 如果 空白的index%row 不等於1 (最左邊那排) 可以向右移動的意思
-//     空白格子左邊就有可移動格子就是index-1 ，取得這個方塊DOM作操作
-// 如果 空白的index%row 不等於0 (最右邊那排) 可以向左移動的意思
-//     空白格子右邊就有可移動格子就是index+1
-// 如果 空白的index/row 不等於0 (最上邊那排) 可以向下移動的意思
-//     空白格子上邊就有可移動格子就是index-row
-// 如果 空白的index/row 不等於(row-1) (最下邊那排) 可以向上移動的意思
-//     空白格子下邊就有可移動格子就是index+row
+    let shuffleTimes = Math.pow(imgBase, 2) * 10;
+
+    for (let i = 0; i < shuffleTimes; i++) {
+        let whiteBlockIndex = localBlock.indexOf(Number(imgBase * imgBase - 1));
+        let randomNum = Math.floor(Math.random() * 4); // 0上 1下 2左 3右
+        //上
+        if (randomNum === 0 && whiteBlockIndex - row >= 0) {
+            let topBlock = document.querySelector(
+                `#b${localBlock[whiteBlockIndex - row]}`
+            );
+            changeBlock(whiteBlockIndex - row, whiteBlockIndex);
+            chageCanvas(topBlock, whiteBlock);
+        }
+        //下
+        else if (
+            randomNum === 1 &&
+            whiteBlockIndex + row <= Math.pow(row, 2) - 1
+        ) {
+            let bottomBlock = document.querySelector(
+                `#b${localBlock[whiteBlockIndex + row]}`
+            );
+            changeBlock(whiteBlockIndex + row, whiteBlockIndex);
+            chageCanvas(bottomBlock, whiteBlock);
+        }
+        //左
+        else if (randomNum === 2 && whiteBlockIndex % row !== 0) {
+            let leftBlock = document.querySelector(
+                `#b${localBlock[whiteBlockIndex - 1]}`
+            );
+            changeBlock(whiteBlockIndex - 1, whiteBlockIndex);
+            chageCanvas(leftBlock, whiteBlock);
+        }
+        //右
+        else if (randomNum === 3 && whiteBlockIndex % row !== row - 1) {
+            let rightBlock = document.querySelector(
+                `#b${localBlock[whiteBlockIndex + 1]}`
+            );
+            changeBlock(whiteBlockIndex + 1, whiteBlockIndex);
+            chageCanvas(rightBlock, whiteBlock);
+        } else {
+            i--;
+        }
+
+        //如果洗排後和答案一樣則重洗。
+        if (
+            i === shuffleTimes - 1 &&
+            localBlock.toString() === baseBlock.toString()
+        ) {
+            i = 0;
+        }
+    }
+});
