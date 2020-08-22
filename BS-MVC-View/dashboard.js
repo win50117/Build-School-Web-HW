01ddaa89ad494c359c1a051885972adaafcaae8f
Cheng Ching Jen <win50117@gmail.com>
2020-08-23 04:29:47 +0800

    加入範例圖表

---
+++ BS-MVC-View/dashboard.js
@@ -0,0 +1,67 @@
+var ctx = document.getElementById("earnings-chart").getContext("2d");
+var myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+        labels: [
+            "一月",
+            "二月",
+            "三月",
+            "四月",
+            "五月",
+            "六月",
+            "七月",
+            "八月",
+            "九月",
+            "十月",
+            "十一月",
+            "十二月",
+        ],
+        datasets: [
+            {
+                label: "# of Votes",
+                data: [
+                    100000,
+                    200000,
+                    232000,
+                    310521,
+                    150354,
+                    300000,
+                    400000,
+                    305100,
+                    246000,
+                    333000,
+                    275000,
+                    255000,
+                ],
+                backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(255, 206, 86, 0.2)",
+                    "rgba(75, 192, 192, 0.2)",
+                    "rgba(153, 102, 255, 0.2)",
+                    "rgba(255, 159, 64, 0.2)",
+                ],
+                borderColor: [
+                    "rgba(255, 99, 132, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(153, 102, 255, 1)",
+                    "rgba(255, 159, 64, 1)",
+                ],
+                borderWidth: 1,
+            },
+        ],
+    },
+    options: {
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true,
+                    },
+                },
+            ],
+        },
+    },
+});
